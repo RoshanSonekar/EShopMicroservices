1e40d2e751c9766b291b5cd7a65bf7369b6547ec
Roshan Sonekar <roshansonekar@gmail.com>
2026-07-22 21:35:52 +0200

    Marten pagination

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,18 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data
+{
+	public class CatalogInitialData : IInitialData
+	{
+		public async Task Populate(IDocumentStore store, CancellationToken cancellationToken)
+		{
+			using var session = store.LightweightSession();
+			if (await session.Query<Product>().AnyAsync())
+				return;
+
+			// Marten UPSERT will cater for existing records
+			session.Store<Product>();
+			await session.SaveChangesAsync();
+		}
+	}
+}
